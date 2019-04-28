27be1247e8f1bda0bedabe0f031099bbfa82c75e
oXoTrio <46646457+oXoTrio@users.noreply.github.com>
2019-04-28 08:47:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on(`ready`, () => {
-    console.log(`Running as ${client.user.tag}`)
-    client.user.setActivity("Minecraft", {type: "PLAYING"});
+// client.on("ready", () => {
+//     console.log(`Running as ${client.user.tag}`)
+//     client.user.setActivity('Im big gay.')
+// });
+client.on("message", message => {
+
+    let msg = message.content.toLowerCase();
+    if (msg.startsWith("?")) {
+        if (message.author.id !== "533549393073405952") return;
+
+        message.delete()
+        let botembed = new Discord.RichEmbed()
+           .setDescription(`**${message.content.slice(1)}**`)
+           .setColor("7289DA")
+           message.channel.send(botembed)
+        //message.channel.send(message.content.slice(1))
+    }
+    if (msg === '') {
+        if (message.author.id !== "533549393073405952") return;
+
+        console.log('test')
+    }
+
 });
 client.login(process.env.BOT_TOKEN);
