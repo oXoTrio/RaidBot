9b7328d507a3ca18ae862a7c4fee2cff27ece169
oXoTrio <46646457+oXoTrio@users.noreply.github.com>
2019-05-19 13:12:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js')
 const client = new Discord.Client();
-// client.on("ready", () => {
-//     console.log(`Running as ${client.user.tag}`)
-//     client.user.setActivity('Im big gay.')
-// });
-client.on("message", message => {
 
-    let msg = message.content.toLowerCase();
-    if (msg.startsWith("'")) {
-        if (message.author.id !== "533549393073405952") return;
+client.on('ready', (guild) => {
 
-        message.delete()
-        let botembed = new Discord.RichEmbed()
-           .setDescription(`**${message.content.slice(1)}**`)
-           .setColor("7289DA")
-           message.channel.send(botembed)
-        //message.channel.send(message.content.slice(1))
-    }
-    if (msg === '') {
-        if (message.author.id !== "533549393073405952") return;
-
-        console.log('test')
-    }
-
+    console.log(`Running client: ${client.user.tag}!`);
+    client.user.setActivity('Advertising for MTS')
 });
+client.on('message', msg => {
+    if(msg.author.id !== '579705916857909249') return;
+
+    if(msg.content === '.') {
+        msg.delete()
+        let embed = new Discord.RichEmbed()
+            .setTitle('Join Mayflower Taxi Service Today!')
+            .setURL("https://discord.gg/xBHtGg4")
+            .setColor("e2b96c")
+            //.setImage("https://cdn.discordapp.com/attachments/579697843720159235/579709661071278111/mts1111.png")
+            .setThumbnail("https://cdn.discordapp.com/attachments/579697843720159235/579709661071278111/mts1111.png")
+            .setFooter("Mayflower Taxi Service", "https://cdn.discordapp.com/attachments/579697843720159235/579709641567502356/MTS11.png")
+            .setDescription("Old company being revamped! Join MTS today and apply!")
+            .setAuthor("Mayflower Taxi Service", "https://cdn.discordapp.com/attachments/579697843720159235/579709641567502356/MTS11.png", "https://discord.gg/xBHtGg4")
+        msg.channel.send(embed)
+        msg.channel.send("https://discord.gg/xBHtGg4")
+    }
+})
+
 client.login(process.env.BOT_TOKEN);
